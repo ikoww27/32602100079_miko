14248590c798804ecba08cfe03216e743733b061
hendraiiirawan <140344575+hendraiiirawan@users.noreply.github.com>
2023-07-27 15:54:53 +0700

    Update ResponseTrait.php

--- API/ResponseTrait.php
+++ API/ResponseTrait.php
@@ -2,7 +2,7 @@
 
 /**
  * This file is part of CodeIgniter 4 framework.
- *
+ *iko
  * (c) CodeIgniter Foundation <admin@codeigniter.com>
  *
  * For the full copyright and license information, please view
